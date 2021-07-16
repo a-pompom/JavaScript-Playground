7ea1983166030ff148734080e1d2df985549d745
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-07-16 20:57:48 +0900

    コントローラ下地処理のテストコードを作成
    CLI操作として、コンソール出力をモック化して検証。
    開始メッセージの出力を捕捉できたことを確認した。

---
+++ TypeScript/appTodoForCLI/tests/controller.test.js
@@ -0,0 +1,15 @@
+import { TodoController } from '../controller';
+import { jest } from '@jest/globals';
+test('開始メッセージが出力されること', () => {
+    // GIVEN
+    const consoleMock = jest.fn();
+    console.log = consoleMock;
+    const expected = TodoController.START_MESSAGE;
+    // WHEN
+    const sut = new TodoController();
+    sut.run();
+    // THEN
+    const actual = consoleMock.mock.calls[0][0];
+    expect(actual).toBe(expected);
+});
+//# sourceMappingURL=controller.test.js.map
