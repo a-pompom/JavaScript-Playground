795449826cd44ff55d4fc9b2cb74aad9b28a3f55
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-08-05 21:37:19 +0900

    アーキテクチャドキュメントを作成

--- TypeScript/appTodoForCLI/document/source/conf.py
+++ TypeScript/appTodoForCLI/document/source/conf.py
@@ -27,7 +27,8 @@ author = 'a-pompom'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx_rtd_dark_mode']
+extensions = ['sphinxcontrib.mermaid', 'myst_parser']
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
